c88e9cfdf8a957e6acb8c2167336c02a22d777b7
Limseokchang <111727297+PsychoLim@users.noreply.github.com>
2023-12-01 11:41:50 +0900

    ScriptableObject 활용 및 Weapon스크립트 생성
    ScripcableObject의 데이터를 참조해서 하나의 프리팹으로
    값을 다르게 만들 수 있게 했습니다.
    캐릭터들의 기본 시작 무기를 만들었습니다. (4개, 추후 값 변경)

--- Assets/PlayerScripts/Shooting.cs
+++ Assets/PlayerScripts/Shooting.cs
@@ -3,17 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum WeaponType
+{
+    Machete,
+    Saw,
+    Balloon,
+    CRTTV,
+
+}
+
 public class Shooting : MonoBehaviour
 {
+    [SerializeField] private List<WeaponData> _weaponsDatas; // 무기의 데이터
+    [SerializeField] private GameObject _weaponPrefab; // 무기의 프리팹
+
     private Controller _controller;
 
     [SerializeField] private Transform _weaponSpawnPoint; // 던질 무기가 생성 될 위치 입니다.
 
     private Vector2 _aimDirection = Vector2.zero; // 어떤 방향으로 던지는 벡터값 입니다.
 
-    private float _weaponSpeed = 5;
 
-    public GameObject tesGameObject; //무기의 프리팹
     private void Awake()
     {
         _controller = GetComponent<Controller>();
@@ -34,16 +44,19 @@ public class Shooting : MonoBehaviour
     }
     private void OnShoot()
     {
-        CreateProjectile();
+        CreateProjectile(WeaponType.Saw);
         
     }
 
-    private void CreateProjectile()
+    private Weapon CreateProjectile(WeaponType type)
     {
 
         Debug.Log("발사");
-
-        GameObject projectile = Instantiate(tesGameObject, _weaponSpawnPoint.position, Quaternion.identity);
-        projectile.GetComponent<Rigidbody2D>().velocity = _aimDirection * Vector2.one * _weaponSpeed;
+        var newWeapon = Instantiate(_weaponPrefab, _weaponSpawnPoint.position, Quaternion.identity).GetComponent<Weapon>();
+        newWeapon.weaponData = _weaponsDatas[(int)type];
+        newWeapon.name = newWeapon.weaponData.WeaponName;
+        newWeapon.GetComponent<SpriteRenderer>().sprite = newWeapon.weaponData.Icon;
+        newWeapon.GetComponent<Rigidbody2D>().velocity = _aimDirection * Vector2.one * newWeapon.weaponData.MoveSpeed;
+        return newWeapon;
     }
 }
